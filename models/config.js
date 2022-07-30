edac37726a04f72a76658a4d7aa7b613b762350e
tanacchi <q111026d@mail.kyutech.jp>
2022-07-30 16:51:59 +0900

    Rename oridinal redis client redisClient -> client

--- models/config.js
+++ models/config.js
@@ -1,13 +1,13 @@
 const redis = require('redis');
 require('dotenv').config();
 
-const redisClient = redis.createClient({
+const client = redis.createClient({
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
     },
 });
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-(async () => { await redisClient.connect() })();
+client.on('error', (err) => console.log('Redis Client Error', err));
+(async () => { await client.connect() })();
 
-module.exports = redisClient;
+module.exports = client;
--- models/progress.js
+++ models/progress.js
@@ -1,18 +1,18 @@
-const redisClient = require('./config');
+const client = require('./config');
 require('dotenv').config();
 
 const getAll = async () => {
-    const results = await redisClient.get('ahi');
+    const results = await client.get('*');
     console.log(results);
 }
 
 const get = async (key) => {
-    const result = await redisClient.get(key);
+    const result = await client.get(key);
     return result;
 }
 
 const set = async (key, value) => {
-    await redisClient.set(key, value);
+    await client.set(key, value);
 }
 
 module.exports = {
