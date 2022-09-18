fa899098deaf29ea3d4c9ceb0af1c2c20adaef63
tanacchi <q111026d@mail.kyutech.jp>
2022-09-18 17:23:55 +0900

    Add tests for standard pattern

--- test/int.spec.ts
+++ test/int.spec.ts
@@ -2,9 +2,33 @@
 import * as request from "supertest";
 import app from "../src/app";
 
-describe("get all", () => {
+const TIMESTAMP = Date.now();
+const testId = `test_${TIMESTAMP}`;
+
+describe("Integration test", () => {
   test("GET:/ が 200 を返す",async () => {
     const response = await request(app).get("/");
-    expect(response)
+    expect(response.statusCode).toBe(200);
   });
-})
+
+  test("POST:/{id} が 204 を返す",async () => {
+    const response = await request(app)
+      .post(`/${testId}`)
+      .send({
+        device: "line",
+        status: "pending",
+        newQuestion: 2,
+      });
+    expect(response.statusCode).toBe(204);
+  });
+
+
+  test("GET:/{id} が 200 を返す",async () => {
+    const response = await request(app).get(`/${testId}`);
+    expect(response.statusCode).toBe(200);
+    const responseData = JSON.parse(response.text);
+    expect(responseData.result.device).toBe("line");
+    expect(responseData.result.status).toBe("pending");
+    expect(responseData.result.questions).toStrictEqual([2]);
+  });
+});
