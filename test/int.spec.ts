2042d701973ce78a824289831b2dd9fb0d069a96
tanacchi <q111026d@mail.kyutech.jp>
2022-09-18 17:59:08 +0900

    Add test for updating user data

--- test/int.spec.ts
+++ test/int.spec.ts
@@ -6,9 +6,11 @@ const TIMESTAMP = Date.now();
 const testId = `test_${TIMESTAMP}`;
 
 describe("Integration test", () => {
-  test("GET:/ が 200 を返す",async () => {
+  test("GET:/ が results を返す",async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
+    const responseData = JSON.parse(response.text);
+    expect(responseData.results).toBeInstanceOf(Array);
   });
 
   test("POST:/{id} が 204 を返す",async () => {
@@ -22,13 +24,30 @@ describe("Integration test", () => {
     expect(response.statusCode).toBe(204);
   });
 
-
-  test("GET:/{id} が 200 を返す",async () => {
+  test("GET:/{id} でデータを取得できる",async () => {
     const response = await request(app).get(`/${testId}`);
     expect(response.statusCode).toBe(200);
     const responseData = JSON.parse(response.text);
     expect(responseData.result.device).toBe("line");
     expect(responseData.result.status).toBe("pending");
     expect(responseData.result.questions).toStrictEqual([2]);
+  });
+
+  test("POST:/{id} でデータを更新できる",async () => {
+    const response1 = await request(app)
+      .post(`/${testId}`)
+      .send({
+        device: "web",
+        status: "asking",
+        newQuestion: 4,
+      });
+    expect(response1.statusCode).toBe(204);
+
+    const response = await request(app).get(`/${testId}`);
+    expect(response.statusCode).toBe(200);
+    const responseData = JSON.parse(response.text);
+    expect(responseData.result.device).toBe("web");
+    expect(responseData.result.status).toBe("asking");
+    expect(responseData.result.questions).toStrictEqual([2, 4]);
   });
 });
